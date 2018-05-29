2aba87b6752c7d659ff0a521021fdb36bdd752bb
Shayan Shokrgozar <shan.shokr@gmail.com>
2018-05-28 23:02:50 -0600

    updating server

--- server.js
+++ server.js
@@ -3,18 +3,15 @@
 // BASE SETUP
 // =============================================================================
 
-
-
-
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
 
+var bodyParser = require('body-parser');
 var Bear     = require('./app/models/bear');
 
 var mongoose   = require('mongoose');
-mongoose.connect(''); // connect to our database here
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database here
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -34,13 +31,32 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
 // more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
