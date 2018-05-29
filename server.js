39de827b2ae037c482119f8f8926d3e2a32e42a3
Shayan Shokrgozar <shan.shokr@gmail.com>
2018-05-29 12:26:05 -0600

    updating after working with Drew

--- server.js
+++ server.js
@@ -4,14 +4,10 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
+var express    = require(‘express’);        // call express
 var app        = express();                 // define our app using express
-
-var bodyParser = require('body-parser');
-var Bear     = require('./app/models/bear');
-
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database here
+var bodyParser = require(‘body-parser’);
+var http = require('http');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -23,50 +19,43 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get(‘/test’, function(req, res) {
+    console.log(req.query.id);
+   res.json({ message: ‘hooray! welcome to our api!’ });  
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.get(‘/update’, function(req, res) {
+    updateFBIMostWanted();
 });
 
 // more routes for our API will happen here
-
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
-
-    // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
-
-        var bear = new Bear();      // create a new instance of the Bear model
-        bear.name = req.body.name;  // set the bears name (comes from the request)
-
-        // save the bear and check for errors
-        bear.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Bear created!' });
-        });
-
-    });
-
+    
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use(‘/api’, router);
+// localhost:8080/api/test -> above function
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log(‘Magic happens on port ’ + port);
 
-
-
-
+function updateFBIMostWanted(){
+    https.get('https://api.fbi.gov/wanted/v1/list', (resp) => {
+  let data = '';
+ 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+}
